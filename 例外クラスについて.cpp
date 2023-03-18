667d0275db740bab15f33e390bdf736b57c19ef8
minami20230128 <south614fruit@yahoo.co.jp>
2023-03-18 18:20:18 +0900

    例外クラスについて 追記

--- 例外クラスについて.cpp
+++ 例外クラスについて.cpp
@@ -1,1 +1,36 @@
 //・例外クラスについて
+//アプリ内で共通の例外処理を作りたい場合、例外クラスを自作する場合がある
+
+#include <exception>
+#include <string>
+
+class myException : std::exception
+{
+    public:
+        myException(const string &msg)
+        {
+            this->message = msg;
+        }
+
+        const char* what() const noexcept
+        {
+            return this->message.c_str();
+        }
+    
+    private:
+        std::string message;
+
+};
+
+int main()
+{
+    try
+    {
+        throw myException("aaa");
+    }
+    catch(const myException& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+}
