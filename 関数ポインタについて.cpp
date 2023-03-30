b711bf28cf5bc20663f4995da7a3b75e27f0e9e5
minami20230128 <south614fruit@yahoo.co.jp>
2023-03-30 20:43:54 +0900

    関数ポインタについて 追記

--- 関数ポインタについて.cpp
+++ 関数ポインタについて.cpp
@@ -5,25 +5,62 @@
 
 // のように宣言する
 
-#include <iostream>
+//#include <iostream>
 
-int add(int& a, int& b)
-{
-    return a + b;
-}
+// int add(int& a, int& b)
+// {
+//     return a + b;
+// }
 
-int main()
-{
-    int a = 1;
-    int b = 2;
-    int (*pAdd)(int&, int&), i;//関数ポインタ(pAdd)、返り値(i)を宣言する
-    pAdd = add;
-    i = (*pAdd)(a, b);//a,bを渡したときの結果を取得
+// int main()
+// {
+//     int a = 1;
+//     int b = 2;
+//     int (*pAdd)(int&, int&), i;//関数ポインタ(pAdd)、返り値(i)を宣言する
+//     pAdd = add;
+//     i = (*pAdd)(a, b);//a,bを渡したときの結果を取得
 
-    std::cout << i << std::endl;
+//     std::cout << i << std::endl;
 
-    return 0;
-}
+//     return 0;
+// }
 
 //出力結果：3
 
+//関数の配列
+
+#include <iostream>
+ int add(int& a, int& b)
+ {
+     return a + b;
+ }
+
+int substract(int &a, int& b)
+{
+    return b - a;
+}
+
+int multipulate(int& a, int& b)
+{
+    return a * b;
+}
+
+int divide(int& a, int& b)
+{
+    return b / a;
+}
+
+ int main()
+ {
+     int a = 1;
+     int b = 2;
+     int (*pCalc[])(int&, int&) = {add, substract, multipulate, divide};//関数の配列を作成
+     
+     for(int i= 0; i< 4; i++)
+     {
+        int result = (*pCalc[i])(a, b);//関数配列にインデックスでアクセス
+        std::cout << result << std::endl;
+     }
+
+     return 0;
+ }
