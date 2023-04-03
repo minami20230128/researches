e098bf622865bccab230d52619817438ce3123f2
minami20230128 <south614fruit@yahoo.co.jp>
2023-04-04 00:26:40 +0900

    Factoryパターンとは 追記

--- Factoryパターンとは.cpp
+++ Factoryパターンとは.cpp
@@ -3,11 +3,46 @@
 //長所：具象クラスへの依存度を減らせる
 
 //例えば、
-//stationallyクラスを継承したscissorクラス、pencilクラス、eraserクラスがあったとする
+//Stationeryクラスを継承したScissorクラス、Pencilクラス、Eraserクラスがあったとする
 //通常各クラスにアクセスするには、
 
-//auto scissor = new scissor();
+//auto scissor = Scissor::Scissor();
 
 //のようにコンストラクタを使用すれば良い。
 
 //しかし、
+
+//if(...) auto stationery = Scissor::Scissor();
+//else if(///) auto stationery = Pencil::Pencil();
+//else if(###) auto stationery = Eraser::Eraser();
+
+//のように、場合によってどのクラスのインスタンスを作るか変わる場合、
+//例えば新たにStationeryクラスを実装するTapeクラスができるとif文を書き足さねばならず、保守性が悪くなる。
+
+//そこで、
+
+public class Stationery
+{
+
+};
+
+public class Scissor:Stationery
+{
+
+}
+
+public class StationeryFactory//Stationeryクラスを継承する下位クラスのインスタンスを生成する「ファクトリークラス」のインターフェイス
+{
+    public:
+        virtual Stationery CreateStationery();
+};
+
+public class ScissorFactory:StationeryFactory//StationeryFactoryクラスを実装する
+{
+    public:
+        Scissor CreateScissor();
+};
+
+//のように、インスタンスを生成するファクトリークラスを作成することで、if文を減らすことができる。
+
+//ただし、記述量はコンストラクタを用いた場合より増える
