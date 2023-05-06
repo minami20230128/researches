9a360f30ac5d2abfc110fdbff837af6b37ca3c52
minami20230128 <south614fruit@yahoo.co.jp>
2023-05-06 23:00:00 +0900

    リスト構造　追加・削除方法追記

--- リスト構造とは.c
+++ リスト構造とは.c
@@ -2,7 +2,7 @@
 //各要素が次の要素を指すポインタを持つリスト
 //ポインタを通して次の要素にアクセスできる
 
-//以下、Book型のリスト構造で二冊目本の題名を表示するコード
+//以下、Book型のリスト構造で1.次のデータのtitleを表示、2.データを插入、3.データを削除するコード
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,6 +27,22 @@ int main()
 
     one->next = two;
 
+    printf("%s", one->next->title);//1.次のデータのtitleを表示
+
+    struct Book* three;
+    three=(struct Book*)malloc(sizeof(struct Book));
+    three->title = (char*)malloc(20);
+    three->title ="third book";
+
+    //2.データを插入
+    one->next = three;//前のデータのポインタ部を挿入したいデータのポインタに置き換える
+    three->next = two;//挿入したデータのポインタ部に後ろのデータのポインタを渡す
+
+    printf("%s", one->next->title);
+
+    //3.データを削除する
+    one->next = two;//削除したいデータの前のデータのポインタ部を、後ろのデータのポインタに置き換える
+
     printf("%s", one->next->title);
 
     free(one);
@@ -35,4 +51,7 @@ int main()
     return 0;
 }
 
-//出力：second book
+//出力：
+//second book
+//third book
+//second book
