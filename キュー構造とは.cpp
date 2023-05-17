1934616ed22a0a9aaa8b69fe1ff091eeb4afaf28
minami20230128 <south614fruit@yahoo.co.jp>
2023-05-17 22:09:42 +0900

    追加

---
+++ キュー構造とは.cpp
@@ -0,0 +1,58 @@
+//・キュー構造とは
+//先入れ先出し方式(FIFO)を取るデータ構造
+//先に入れたデータが先に取り出される
+//エンキュー...データを挿入すること
+//デキュー...データを削除すること
+
+#include <iostream>
+#define MAX 10
+
+class Queue
+{
+    int data[MAX];
+    int idx;
+
+    public:
+        Queue()
+        {
+            this->idx = 0;
+        }
+        void enqueue(int number)
+        {
+            this->data[this->idx] = number;
+            idx += 1;
+        }
+
+        int dequeue()
+        {
+            auto ret = data[0];
+            idx -= 1;
+
+            for(int i = 1; i < sizeof(this->data) / sizeof(int) - 1; i++)
+            {
+                data[i - 1] = data[i];
+            }
+
+            return ret;
+        }
+};
+
+int main()
+{
+    auto queue = Queue();
+    //エンキュー
+    queue.enqueue(10);
+    queue.enqueue(20);
+
+    //デキュー
+    std::cout << queue.dequeue() << std::endl;
+    std::cout << queue.dequeue() << std::endl;
+
+    return 0;
+}
+
+//出力結果：
+//10
+//20
+
+//先にエンキューした10が先に取り出されていることがわかる
