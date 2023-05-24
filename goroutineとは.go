6b8fe275630fc2b411582913dfed67389f91fa7d
minami20230128 <south614fruit@yahoo.co.jp>
2023-05-24 22:00:30 +0900

    追加

---
+++ goroutineとは.go
@@ -0,0 +1,66 @@
+// ・goroutineとは
+// Go言語におけるプログラムの基本的な構成単位
+// main関数はメインゴルーチンと言われる
+
+// 用語
+// channel...同時実行するgoroutineをつなぐパイプ
+// channelを通して、あるgoroutineから別のgoroutineに値を送信することができる
+
+// 以下は。main関数とwelcomeback関数を非同期実行するコード例
+
+// package main
+
+// import (
+//     "fmt"
+//     "time"
+// )
+
+// func welcomeBack() {
+//     fmt.Println("おかえり")
+// }
+
+// func main() {
+//     fmt.Println("ただいま")
+//     go welcomeBack()
+//     time.Sleep(50 * time.Millisecond)
+// }
+
+// 実行結果:
+// (そのまま実行した場合)
+// ただいま
+// おかえり
+// // (23行目を消した場合)
+// ただいま
+
+// 23行目の処理がないと、welcomeback関数の前にmain関数が終了してしまう
+// しかし、毎度終了までの時間を測って指定するのは面倒である
+// そこで sync.WaitGroup を使用すれば、複数のgoroutineの終了を待つことができる
+
+package main
+
+import (
+    "fmt"
+    "sync"
+)
+
+func welcomeBack() {
+    fmt.Println("おかえり")
+}
+
+func main() {
+    fmt.Println("ただいま")
+
+    var wg sync.WaitGroup // WaitGroupの生成
+    wg.Add(1)             // カウンタをインクリメント
+
+    go func() {
+        welcomeBack()
+        wg.Done() // カウンタをデクリメント
+    }()
+
+    wg.Wait() // mainのゴールールは新規のゴールーチンが完了するのを待つ
+}
+
+//実行結果
+//ただいま
+//おかえり
