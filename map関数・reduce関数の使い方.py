cc7aa8a0d8d000befb31bd0c99e4d11b04e7b10c
minami20230128 <south614fruit@yahoo.co.jp>
2023-06-08 00:00:56 +0900

    修正

--- map関数・reduce関数の使い方.py
+++ map関数・reduce関数の使い方.py
@@ -5,14 +5,9 @@
 def double(num):
    return num * 2
 
-list = [1, 2, 3, 4, 5]
-doubled_list = map(double, list)#一行で全要素にdouble関数を反映させることができる
-for num in doubled_list:
-    print(num)
+numberlist = [1, 2, 3, 4, 5]
+doubled_list = map(double, numberlist)#一行で全要素にdouble関数を反映させることができる
+print(list(doubled_list))#map関数が返すのはmapオブジェクトなので、list型に直す
 
 #実行結果
-#2
-#4
-#6
-#8
-#10
+#[2, 4, 6, 8, 10]
