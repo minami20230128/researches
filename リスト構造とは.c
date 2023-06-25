b7cc594aa3cee0ed38b8d82a3fa4297b64bd13a8
minami20230128 <south614fruit@yahoo.co.jp>
2023-06-25 19:19:00 +0900

    修正

--- リスト構造とは.c
+++ リスト構造とは.c
@@ -25,9 +25,11 @@ int main()
     two->title = (char*)malloc(20);
     two->title="second book";
 
+    //1.リスト構造を作る
     one->next = two;
+    two->next = NULL;//最後のデータのポインタにはNULLを入れる
 
-    printf("%s", one->next->title);//1.次のデータのtitleを表示
+    printf("%s", one->next->title);//*1
 
     struct Book* three;
     three=(struct Book*)malloc(sizeof(struct Book));
@@ -38,12 +40,12 @@ int main()
     one->next = three;//前のデータのポインタ部を挿入したいデータのポインタに置き換える
     three->next = two;//挿入したデータのポインタ部に後ろのデータのポインタを渡す
 
-    printf("%s", one->next->title);
+    printf("%s", one->next->title);//*2
 
     //3.データを削除する
     one->next = two;//削除したいデータの前のデータのポインタ部を、後ろのデータのポインタに置き換える
 
-    printf("%s", one->next->title);
+    printf("%s", one->next->title);//*3
 
     free(one);
     free(two);
@@ -52,6 +54,18 @@ int main()
 }
 
 //出力：
-//second book
-//third book
-//second book
+//second book(*1)
+//third book(*2)
+//second book(*3)
+
+//このように、各要素が次の要素へのポインタしか持たないリストを「単方向リスト」という
+//次の要素のポインタしかないので、前のデータに遡ることはできない
+
+//一方、各要素が前後の要素のポインタを持つリストを「双方向リスト」といい、以下のように実現する
+
+// struct Book
+// {
+//     char* title;
+//     struct Book* next;
+//     struct Book* prev;
+// };
