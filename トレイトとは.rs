31f56b69e4ac88a1f31335782d4fece9f233f07e
minami20230128 <south614fruit@yahoo.co.jp>
2024-03-26 21:03:49 +0900

    途中

--- トレイトとは.rs
+++ トレイトとは.rs
@@ -3,16 +3,51 @@
 //Interfaceと異なり、具象メソッドをメンバーに持つことができる
 //これにより、異なるクラスの共通処理をひとまとめにすることができる
 
-//たとえば、以下はある店の夏期と通常の割引率を求めるコードである
+//たとえば、以下はある店の夏期と通常期の割引率を求めるコードである
+//この店には客の年齢を割引率に反映する独自のシステムがあり、季節にかかわらず常に同じロジックで計算される
+//このようなとき、年齢による割引率の計算ロジックをtraitで実装し、夏期と通常期のクラスに継承させることで共通化を図ることができる
 
-
-
-pub struct regular_discounted_price
+pub struct RegularDiscountedPrice
 {
-
+    
 }
 
-pub struct summer_discounted_price
+impl RegularDiscountedPrice
 {
+    pub fn get_discounted_price(price : i32) -> i32{
+        const DISCOUNT_RATE : f64 = 0.1;
+        return DISCOUNT_RATE * price;
+    }
+}
 
+pub struct SummerDiscountedPrice
+{
+    
+}
+
+impl SummerDiscountedPrice
+{
+    pub fn get_discounted_price(price : i32) -> i32{
+        const DISCOUNT_RATE : f64 = 0.12;
+        return DISCOUNT_RATE * price;
+    }
+}
+
+trait DiscountRateByAge
+{
+    fn get_discounted_price(age : i32) -> f32;
+}
+
+impl DiscountRateByAge for RegularDiscountedPrice
+{
+    fn get_discounted_price(age : i32) -> f32{
+        return 1 + age / 100;
+    }
+}
+
+impl DiscountRateByAge for SummerDiscountedPrice
+{
+    fn get_discounted_price(age : i32) -> f32{
+        return 1 + age / 100;
+    }
 }
