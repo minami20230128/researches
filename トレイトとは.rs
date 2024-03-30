124c57a00fdd63ddba109ec53b03708843fa9c26
minami20230128 <south614fruit@yahoo.co.jp>
2024-03-30 22:57:32 +0900

    途中

--- トレイトとは.rs
+++ トレイトとは.rs
@@ -17,7 +17,7 @@ impl RegularDiscountedPrice
     pub fn get_discounted_price(&self, age : i32, price : i32) -> i32{
         let rate_by_age = self.get_discount_rate(age);
         const DISCOUNT_RATE : f32 = 0.1;
-        return (1 - DISCOUNT_RATE * rate_by_age) * price;
+        return (1.0 - DISCOUNT_RATE * rate_by_age * (price as f32)) as i32;
     }
 }
 
@@ -31,7 +31,7 @@ impl SummerDiscountedPrice
     pub fn get_discounted_price(&self, age : i32, price : i32) -> i32{
         let rate_by_age = self.get_discount_rate(age);
         const DISCOUNT_RATE : f32 = 0.12;
-        return (1 - DISCOUNT_RATE * rate_by_age) * price;
+        return (1.0 - DISCOUNT_RATE * rate_by_age * (price as f32)) as i32;
     }
 }
 
@@ -43,21 +43,23 @@ trait DiscountRateByAge
 impl DiscountRateByAge for RegularDiscountedPrice
 {
     fn get_discount_rate(&self, age : i32) -> f32{
-        return 1 + age / 100;
+        return (1 + age / 100) as f32;
     }
 }
 
 impl DiscountRateByAge for SummerDiscountedPrice
 {
     fn get_discount_rate(&self, age : i32) -> f32{
-        return 1 + age / 100;
+        return (1 + age / 100) as f32;
     }
 }
 
 fn main()
 {
-    let regular = RegularDiscountedPrice{rate_by_age : 0};
-    let summer = SummerDiscountedPrice{rate_by_age : 0};
+    const DEFAULT_RATE_BY_AGE : f32 = 0.0;
+
+    let regular = RegularDiscountedPrice{rate_by_age : DEFAULT_RATE_BY_AGE};
+    let summer = SummerDiscountedPrice{rate_by_age : DEFAULT_RATE_BY_AGE};
 
     let age = 20;
     let price = 400;
