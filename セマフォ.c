6947ed2e34de38350d1cb040a4ce38f5aac7e713
minami20230128 <south614fruit@yahoo.co.jp>
2024-06-10 23:35:33 +0900

    途中

--- セマフォ.c
+++ セマフォ.c
@@ -7,17 +7,29 @@
 //「プロセス1がファイルに書き込んだ内容をプロセス2が読み出す」という同期処理を行う時、
 //プロセス1がセマフォを減じておくことでプロセス2を待たせることができる
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/sem.h>
+#include <sys/types.h>
+#define PJ_ID 1
 
 int main()
 {
-    FILE* fp;
-    char* path = "./test.txt";
-    if((fp = fopen(path, 'w')) == NULL)
+    int ch;
+    const char* PATH = "./test.txt";
+
+    key_t key = ftok(PATH, PJ_ID);
+
+    int semid = semget(key, 1, 0666);
+    if(semid  == -1)
     {
-        perror('main : fopen ');
-        exit(EXIT_FAILURE);
+        printf("error");
+
+        return -1;
     }
+
+    printf("sid = %d", semid);
+
     return 0;
 }
 
